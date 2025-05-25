457839f6e97fd2c6bdf6bbd63d190f7d92aa6547
RevanthDiviti <145599691+RevanthDiviti@users.noreply.github.com>
2025-05-25 12:34:00 +0530

    completed with responsive screen

--- Contact_Us/src/components/Hero/Hero.jsx
+++ Contact_Us/src/components/Hero/Hero.jsx
@@ -3,23 +3,37 @@ import "./Hero.css";
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <div className="contact-us">
-        <h1>CONTACT US</h1>
-        <p>
-          LET’S CONNECT: WE’RE HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU!
-          WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT , YOU CAN
-          REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE,
-          EMAIL, OR SOCIAL MEDIA.{" "}
-        </p>
-      </div>
-      <div className="details">
-        <div className="form"></div>
-        <div className="image">
-          <img src="../public/Images/service.svg" alt="" />
+    <>
+      <div className="hero">
+        <div className="contact-us">
+          <h1>CONTACT US</h1>
+          <p>
+            LET’S CONNECT: WE’RE HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU!
+            WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT , YOU CAN
+            REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE,
+            EMAIL, OR SOCIAL MEDIA.{" "}
+          </p>
+        </div>
+        <div className="details">
+          <div className="form">
+            <span className="chat-call">
+              <button>VIA CHAT SUPPORT</button>
+              <button>VIA CALL</button>
+            </span>
+            <button>VIA EMAIL FORM</button>
+            <input type="text" name="" id="" placeholder="NAME" />
+            <input type="text" name="" id="" placeholder="E-MAIL" />
+            <textarea name="" id="" placeholder="TEXT"></textarea>
+            <div className="submit-btn-container">
+              <button id="submit-btn">Submit</button>
+            </div>
+          </div>
+          <div className="image">
+            <img src="../public/Images/service.svg" alt="" />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Contact_Us
+
+https://www.figma.com/design/rephrU2FVgN8MFz6XhnP51/Learn-React-with-10-Projects?node-id=2-5&p=f&t=L3P7Vv3z1LhTt9c3-0
